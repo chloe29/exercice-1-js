27fa66872ddd9481d5226d1af6219d3e321d66c8
lavisse <chloe.lavisse80@gmail.com>
2018-10-05 16:33:41 +0200

    envoi des fichiers

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+déclaration de la variable name
+initialisée avec une chaine de caractère "string"
+*/
+var name = "Bonjour !";
+/*
+affichage de ma variable name dans un boite de dialogue
+que j'appelle avec la fonction alert
+*/
+  alert(name);
